d935ea6a4c7f4834e9ccaeddb4afdeb3ef3c5c9b
Kyle Machulis <kyle@nonpolynomial.com>
2018-01-06 00:18:48 -0800

    Add tutorial code, sugarcube files, and makefile

---
+++ src/sugarcube2-macros.js
@@ -0,0 +1,105 @@
+/*
+  External script loading module.
+  Taken from https://twinery.org/forum/discussion/6628/sugarcube-2-9-0-including-external-javascript-libraries
+*/
+(() => {
+  'use strict';
+
+  const buttplugLoadingPromise = importScripts('https://cdn.jsdelivr.net/npm/buttplug@0.4.0/dist/web/buttplug.min.js');
+
+  // Map multiple payloads from child tags to an es6 <string, object> map.
+  function mapPayloads(payloads) {
+    const payloadMap = new Map();
+    for (const payload of payloads) {
+      payloadMap.set(payload.name, payload);
+    }
+    return payloadMap;
+  }
+
+  // The Buttplug Client needs to live in macro definition scope (basically, as
+  // a private scope variable), but not in Twine State scope. Adding it as part
+  // of State.variables causes weird things to happen between passages and event
+  // listeners to not register correctly, and the client object shouldn't be
+  // serialized into history anyways. This does make management of the object
+  // and devices a bit odd, but this is a sex toy library for an interactive
+  // fiction engine. We started at odd and headed outward from there.
+	let bpClient;
+
+  Macro.add("buttplugloaded", {
+    tags: null,
+    handler() {
+      buttplugLoadingPromise.then(() => Wikifier.wikifyEval(this.payload[0].contents.trim()));
+    }
+  });
+
+  Macro.add("buttplugconnectlocal", {
+    tags: ["connecting", "success", "failure"],
+    async handler() {
+      const payloadMap = mapPayloads(this.payload);
+      // Run the connecting block before actually trying to connect
+      Wikifier.wikifyEval(payloadMap.get("connecting").contents);
+      // TODO Let user name client as argument
+      State.variables.bpClient = new Buttplug.ButtplugClient("Twine Buttplug Client");
+		  bpClient = State.variables.bpClient;
+
+      try {
+        await bpClient.ConnectLocal();
+        Wikifier.wikifyEval(payloadMap.get("success").contents);
+      } catch (e) {
+        Wikifier.wikifyEval(payloadMap.get("failure").contents);
+      }
+    }
+  });
+
+  Macro.add("buttplugstartscanning", {
+    async handler() {
+      if (bpClient === undefined) {
+        return this.error("Trying to run scan without a connection!");
+      }
+      await bpClient.StartScanning();
+    }
+  });
+
+  Macro.add("buttplugstopscanning", {
+    async handler() {
+      if (bpClient === undefined) {
+        return this.error("Trying to run scan without a connection!");
+      }
+      await bpClient.StopScanning();
+    }
+  });
+
+  Macro.add("buttplugdeviceadded", {
+		tags: null,
+    handler() {
+      if (bpClient === undefined) {
+        return this.error("We need a client object!");
+      }
+      bpClient.addListener('deviceadded', (device) => {
+				State.temporary.device = device;
+        Wikifier.wikifyEval(this.payload[0].contents.trim());
+      });
+    }
+  });
+
+  Macro.add("buttplugdeviceremoved", {
+		tags: null,
+    handler() {
+      if (bpClient === undefined) {
+        return this.error("We need a client object!");
+      }
+      bpClient.addListener('deviceremoved', (device) => {
+				State.temporary.device = device;
+        Wikifier.wikifyEval(this.payload[0].contents.trim());
+      });
+    }
+  });
+
+  Macro.add("buttplugdisconnect", {
+		tags: null,
+    handler() {
+      // We currently have no way to detect this. WTF is wrong with me.
+      // https://github.com/metafetish/buttplug-js/issues/64
+    }
+  });
+})();
