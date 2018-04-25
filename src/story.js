3c759904df0aada76f536a023217871add4ca86a
Kyle Machulis <kyle@nonpolynomial.com>
2018-04-24 21:26:47 -0700

    Try adding piwik tracking to tutorial

--- src/story.js
+++ src/story.js
@@ -2,3 +2,22 @@
 
 // Set wikifier to output <p> tags.
 Config.cleanupWikifierOutput = true;
+
+var _paq = _paq || [];
+_paq.push(['trackPageView']);
+_paq.push(['enableLinkTracking']);
+(function() {
+  var u=(('https:' == document.location.protocol) ? 'https' : 'http') + '://apps.nonpolynomial.com/piwik/';
+  // Ensure URL ends with slash
+  if (u.slice(-1) !== '/') u+='/';
+  _paq.push(['setTrackerUrl', u+'piwik.php']);
+  _paq.push(['setSiteId', 10]);
+  var d=document,
+      g=d.createElement('script'),
+      s=d.getElementsByTagName('script')[0];
+  g.type='text/javascript';
+  g.defer=true;
+  g.async=true;
+  g.src=u+'piwik.js';
+  s.parentNode.insertBefore(g,s);
+})();
