5d8c8adb62e542a2224058406b199307b0b55e64
Kyle Machulis <kyle@nonpolynomial.com>
2018-04-25 11:48:21 -0700

    Try fixing piwik again

--- src/piwik.js
+++ src/piwik.js
@@ -1,21 +1,13 @@
 $(document).on(":passageend", (ev) => {
   var _paq = _paq || [];
   _paq.push(['setDocumentTitle', passage()]);
-  _paq.push(['trackPageView']);
-  _paq.push(['enableLinkTracking']);
+  _paq.push(["trackPageView"]);
+  _paq.push(["enableLinkTracking"]);
   (function() {
-    var u=(('https:' == document.location.protocol) ? 'https' : 'http') + '://apps.nonpolynomial.com/p/';
-    // Ensure URL ends with slash
-    if (u.slice(-1) !== '/') u+='/';
-    _paq.push(['setTrackerUrl', u+'js/']);
-    _paq.push(['setSiteId', 10]);
-    var d=document,
-        g=d.createElement('script'),
-        s=d.getElementsByTagName('script')[0];
-    g.type='text/javascript';
-    g.defer=true;
-    g.async=true;
-    g.src=u+'js/';
-    s.parentNode.insertBefore(g,s);
+    var u=(("https:" == document.location.protocol) ? "https://apps.nonpolynomial.com/p/" : "http://apps.nonpolynomial.com/p/");
+    _paq.push(["setTrackerUrl", u+"js/"]);
+    _paq.push(["setSiteId", "10"]);
+    var d=document, g=d.createElement("script"), s=d.getElementsByTagName("script")[0]; g.type="text/javascript";
+    g.defer=true; g.async=true; g.src=u+"js/"; s.parentNode.insertBefore(g,s);
   })();
 });
