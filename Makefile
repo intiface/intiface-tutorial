99116f4152ff22914469d96ac03a2e42cd70f83c
Kyle Machulis <kyle@nonpolynomial.com>
2018-04-25 17:26:08 -0700

    Move loading of piwik to head in order to fix init race conditions

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 all: clean
 	mkdir -p build
-	TWEEGO_PATH=. $(GOPATH)/bin/tweego -o build/index.html src buttplug-twine/src assets/images
+	TWEEGO_PATH=. $(GOPATH)/bin/tweego -o build/index.html --head=util/piwik.html src buttplug-twine/src assets/images
 	cp assets/screencaps/* build
 
 clean:
--- src/story.js
+++ src/story.js
@@ -3,27 +3,10 @@
 // Set wikifier to output <p> tags.
 Config.cleanupWikifierOutput = true;
 
-// The _paq variable needs to live in the window scope, because piwik just
-// expects a global _paq. Therefore, run setup on window to make sure the global
-// is produced there, then update page visits on passageend (after we've made
-// sure everything loaded.)
-(function () {
-  var _paq = _paq || [];
-  _paq.push(['enableLinkTracking']);
-  (function() {
-    var u="https://apps.nonpolynomial.com/p/";
-    _paq.push(['setTrackerUrl', u+'js/']);
-    _paq.push(['setSiteId', '10']);
-    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-    g.type='text/javascript';
-    g.async=true;
-    g.defer=true;
-    g.src=u+'js/';
-    s.parentNode.insertBefore(g,s);
-  })();
-}).call(window);
-
-$(document).on(":passageend", (ev) => {
-  _paq.push(['trackPageView', passage()]);
+$(document).on(":passageend", function (ev) {
+  if ("undefined" !== typeof(_paq)) {
+    console.log(_paq);
+    _paq.push(['trackPageView', passage()]);
+  }
 });
 
