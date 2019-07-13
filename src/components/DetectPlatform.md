fe4ff33c8a6b1b6e9c6d46e25efadd8564e3b27a
Kyle Machulis <kyle@nonpolynomial.com>
2019-07-13 16:08:59 -0700

    chore: Convert to using markdown embedding
    Inherit from markdown as a component, so we can just use markdown as
    the component template.

---
+++ src/components/DetectPlatform.md
@@ -0,0 +1,7 @@
+Let's see what OS you're running.
+
+It looks like you're running {{ OSName }} {{ BrowserName }} {{ 2+3 }}.
+
+{{ Test }}
+
+<router-link to="choose-connection">Next, we'll choose a connection type.</router-link>
--- src/components/DetectPlatform.ts
+++ src/components/DetectPlatform.ts
@@ -1,9 +1,10 @@
 import Vue from "vue";
 import Platform from "platform";
 import { Component } from "vue-property-decorator";
+const DetectPlatformText = require("./DetectPlatform.md").vue.component;
 
 @Component({})
-export default class DetectPlatform extends Vue {
+export default class DetectPlatform extends Vue.extend(DetectPlatformText) {
   private OSName = Platform.os;
   private BrowserName = Platform.name;
 }
