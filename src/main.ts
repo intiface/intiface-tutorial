949b59aabc94f7ca5487c2044f982b523f8be9dd
Kyle Machulis <kyle@nonpolynomial.com>
2019-08-21 20:35:34 -0700

    build: Update dependencies

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,12 @@ import Vue from "vue";
 import "./plugins/vuetify";
 import App from "./App.vue";
 import router from "./router";
+import vuetify from "./plugins/vuetify";
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
+  vuetify,
   render: (h) => h(App),
 }).$mount("#app");
