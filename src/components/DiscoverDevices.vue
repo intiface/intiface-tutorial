79d06cd63ad3cd48ab30eed49e2c16d0ebb72825
Kyle Machulis <kyle@nonpolynomial.com>
2019-08-22 21:10:16 -0700

    chore: Finish device scan implementation, start on control devices

--- src/components/DiscoverDevices.vue
+++ src/components/DiscoverDevices.vue
@@ -1,10 +1,19 @@
 <template>
   <div class="markdown-body">
     Now let's find some devices!
-
+    <br/>
+    <br/>
     <v-btn @click="FindDevices">Find devices</v-btn>
-
-    <router-link to="test-devices"></router-link>
+    <br/>
+    <br/>
+    <v-list dense>
+      <v-list-item
+        v-for="device in devices"
+        key="device.Index"
+        v-text="device.Name">
+      </v-list-item>
+    </v-list>
+    <router-link to="control-devices" v-if="devices.length > 0">On to controlling devices!</router-link>
   </div>
 </template>
 
