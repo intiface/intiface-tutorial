8a00a8d00251ec2e0c5cdd67074b505ce2f91b7b
Kyle Machulis <kyle@nonpolynomial.com>
2019-08-30 21:18:25 -0700

    feat: Add basic vibration controls

--- src/components/ControlDevices.vue
+++ src/components/ControlDevices.vue
@@ -1,5 +1,17 @@
 <template>
+  <v-container class="markdown-body">
+    <v-list dense v-for="device in devices">
+      <v-list-item
+        :key="device.Index"
+        v-text="device.Name">
+      </v-list-item>
+      <div v-if="device.AllowedMessages.indexOf('VibrateCmd') !== -1" @click="Vibrate(device)">Vibrate</div>
+      <div v-if="device.AllowedMessages.indexOf('RotateCmd') !== -1">Rotate</div>
+      <div v-if="device.AllowedMessages.indexOf('LinearCmd') !== -1">Oscillate</div>
+    </v-list>
+    <router-link to="conclusion">Let's wrap up and get you on your way.</router-link>
+  </v-container>
 </template>
 
-<script lang="ts" src"./ControlDevices.ts">
+<script lang="ts" src="./ControlDevices.ts">
 </script>
