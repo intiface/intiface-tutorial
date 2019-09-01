dce434163f77f42a33c313e5895ae28c69a6b870
Kyle Machulis <kyle@nonpolynomial.com>
2019-09-01 14:29:40 -0700

    build: Fix terser output to deal with buttplug classname resolution

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,14 @@
 module.exports = {
+  configureWebpack: config => {
+    if (process.env.NODE_ENV === 'production') {
+      const terserWebpackPlugin = config.optimization.minimizer[0];
+      const terserOptions = terserWebpackPlugin.options.terserOptions;
+      terserOptions.compress['keep_fnames'] = true;
+      terserOptions.compress['keep_classnames'] = true;
+      terserOptions.mangle['keep_fnames'] = true;
+      terserOptions.mangle['keep_classnames'] = true;
+    }
+  },
   chainWebpack: config => {
     config.module
       .rule('markdown')
