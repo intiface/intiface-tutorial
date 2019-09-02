10819483ba2e56f6c02d6fc39f3f8a1dc27c989e
Kyle Machulis <kyle@nonpolynomial.com>
2019-09-01 19:58:40 -0700

    feat: Add page title changes to router moves

--- src/router.ts
+++ src/router.ts
@@ -16,7 +16,9 @@ import Conclusion from "./components/Conclusion";
 
 Vue.use(Router);
 
-export default new Router({
+const DEFAULT_TITLE = "Intiface Tutorial";
+
+const router = new Router({
   mode: "history",
   base: process.env.BASE_URL,
   routes: [
@@ -24,66 +26,111 @@ export default new Router({
       path: "/",
       name: "intro",
       component: Intro,
+      meta: {
+        title: "Intiface Tutorial - Intro",
+      },
     },
     {
       path: "/choose-connection",
       name: "choose-connection",
       component: ChooseConnection,
+      meta: {
+        title: "Intiface Tutorial - Choose Connection",
+      },
     },
     {
       path: "/download-intiface",
       name: "download-intiface",
       component: DownloadIntiface,
+      meta: {
+        title: "Intiface Tutorial - Download Intiface Desktop",
+      },
     },
     {
       path: "/setup-intiface-desktop",
       name: "setup-intiface-desktop",
       component: SetupIntifaceDesktop,
+      meta: {
+        title: "Intiface Tutorial - Setup Intiface Desktop",
+      },
     },
     {
       path: "/create-browser-connection",
       name: "create-browser-connection",
       component: CreateBrowserConnection,
+      meta: {
+        title: "Intiface Tutorial - Create Browser Connection",
+      },
     },
     {
       path: "/create-intiface-connection",
       name: "create-intiface-connection",
       component: CreateIntifaceConnection,
+      meta: {
+        title: "Intiface Tutorial - Create Intiface Desktop Connection",
+      },
     },
     {
       path: "/discover-devices",
       name: "discover-devices",
       component: DiscoverDevices,
+      meta: {
+        title: "Intiface Tutorial - Discover Devices",
+      },
     },
     {
       path: "/control-devices",
       name: "control-devices",
       component: ControlDevices,
+      meta: {
+        title: "Intiface Tutorial - Control Devices",
+      },
     },
     {
       path: "/support",
       name: "support",
       component: Support,
+      meta: {
+        title: "Intiface Tutorial - Support",
+      },
     },
     {
       path: "/welcome",
       name: "welcome",
       component: Welcome,
+      meta: {
+        title: "Intiface Tutorial - Welcome",
+      },
     },
     {
       path: "/terms",
       name: "terms",
       component: Terms,
+      meta: {
+        title: "Intiface Tutorial - Terms",
+      },
     },
     {
       path: "/overview",
       name: "overview",
       component: Overview,
+      meta: {
+        title: "Intiface Tutorial - Overview",
+      },
     },
     {
       path: "/conclusion",
       name: "conclusion",
       component: Conclusion,
+      meta: {
+        title: "Intiface Tutorial - Conclusion",
+      },
     },
   ],
 });
+
+router.afterEach((to, from) => {
+  document.title = to.meta.title || DEFAULT_TITLE;
+});
+
+export default router;
