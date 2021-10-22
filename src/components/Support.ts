64c222c99ac975c2f42a8060db94393382f080aa
Kyle Machulis <kmachulis@grabango.com>
2021-10-22 14:53:08 -0700

    Remove zendesk links

--- src/components/Support.ts
+++ src/components/Support.ts
@@ -5,18 +5,8 @@ const SupportText = require("./Support.md").vue.component;
 @Component({})
 export default class Support extends Vue.extend(SupportText) {
   public mounted() {
-    if (document.getElementById("launcher") === null) {
-      const body = document.getElementsByTagName("body")[0];
-      const script = document.createElement("script");
-      script.setAttribute("id", "ze-snippet");
-      script.setAttribute("src", "https://static.zdassets.com/ekr/snippet.js?key=aba3172c-0357-430b-889b-ee229173abf2");
-      body.appendChild(script);
-    } else {
-      document.getElementById("launcher")!.style.display = "block";
-    }
   }
 
   public beforeDestroy() {
-    document.getElementById("launcher")!.style.display = "none";
   }
 }
